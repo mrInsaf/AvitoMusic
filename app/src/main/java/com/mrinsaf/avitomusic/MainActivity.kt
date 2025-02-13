0b5578beec7499c39423a33327a44052a21ccf7f
mrinsaf <mr.insaf@list.ru>
2025-02-13 14:38:26 +0300

    Добавил отступы контенту основного компонента

--- app/src/main/java/com/mrinsaf/avitomusic/MainActivity.kt
+++ app/src/main/java/com/mrinsaf/avitomusic/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -43,16 +41,23 @@ fun MusicApp() {
     Scaffold(
         bottomBar = {
             BottomNavigationBar(navController)
-        }
+        },
     ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = "downloaded_tracks",
-            modifier = Modifier.padding(innerPadding)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(12.dp)
         ) {
-            composable("downloaded_tracks") { DownloadedTracksScreen(navController) }
-//            composable("api_tracks") { ApiTracksScreen(navController) }
-//            composable("player") { PlayerScreen(navController) }
+            NavHost(
+                navController = navController,
+                startDestination = "downloaded_tracks",
+                modifier = Modifier.fillMaxSize()
+            ) {
+                composable("downloaded_tracks") { DownloadedTracksScreen(navController) }
+                // composable("api_tracks") { ApiTracksScreen(navController) }
+                // composable("player") { PlayerScreen(navController) }
+            }
         }
     }
 }
