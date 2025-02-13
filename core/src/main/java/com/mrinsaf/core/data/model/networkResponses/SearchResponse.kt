f83281c3443c2df94b16fcb34a6e2b701860615b
mrinsaf <mr.insaf@list.ru>
2025-02-13 15:49:35 +0300

    Удалил неправильные модели

--- core/src/main/java/com/mrinsaf/core/data/model/networkResponses/SearchResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.mrinsaf.core.data.model.networkResponses
-
-import com.mrinsaf.core.data.model.Track
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class SearchResponse(
-    val data: List<Track>
-)
