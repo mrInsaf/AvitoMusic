90449f3c5aabcb80f26222b2ddcb1325ddf62a4b
mrinsaf <mr.insaf@list.ru>
2025-02-15 22:59:56 +0300

    Добавил кастомный заголовок для TracksScreen

--- core/src/main/java/com/mrinsaf/core/ui/screens/TracksScreen.kt
+++ core/src/main/java/com/mrinsaf/core/ui/screens/TracksScreen.kt
@@ -1,14 +1,14 @@
 package com.mrinsaf.core.ui.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.mrinsaf.core.data.model.TrackUiModel
 import com.mrinsaf.core.ui.components.TrackItem
@@ -16,19 +16,19 @@ import com.mrinsaf.core.ui.components.TrackItem
 @Composable
 fun TracksScreen(
     tracks: List<TrackUiModel>,
+    title: String,
     query: String,
     onQueryChange: (String) -> Unit,
     onTrackClick: (TrackUiModel) -> Unit
 ) {
     Column {
-        TextField(
-            value = query,
-            onValueChange = onQueryChange,
-            label = { Text("Поиск") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 12.dp)
         )
+
 
         LazyColumn {
             items(tracks) { track ->
@@ -42,3 +42,5 @@ fun TracksScreen(
         }
     }
 }
+
+
