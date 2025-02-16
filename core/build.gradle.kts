94c60430ca450ef25277df56fd09e7d6bdb7281c
mrinsaf <mr.insaf@list.ru>
2025-02-16 19:48:07 +0300

    добавил зависимостей

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 dependencies {
     implementation("io.coil-kt:coil:2.4.0")
     implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation ("io.coil-kt:coil-gif:2.4.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
